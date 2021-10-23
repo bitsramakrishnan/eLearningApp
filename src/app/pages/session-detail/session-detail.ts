fd3d5eab627f7ce127faf0ba68944712f19f5f62
RamakrishnanSelvaraj <2020mt93156@wilp.bits-pilani.ac.in>
2021-10-23 21:29:52 +0530

    Course related display changes done and  Updated with react JS json

--- src/app/pages/session-detail/session-detail.ts
+++ src/app/pages/session-detail/session-detail.ts
@@ -11,6 +11,7 @@ import { UserData } from '../../providers/user-data';
 })
 export class SessionDetailPage {
   sessionId: any = '';
+  selectedCourseName: any = '';
   session: any;
   isFavorite = false;
   defaultHref = '';
@@ -19,27 +20,119 @@ export class SessionDetailPage {
     courseName: 'Angular',
     id: '15',
     syllabus: [
+
       {
         title: 'Lesson 01 - Features of TypeScript',
         line1: 'Introduction',
         line2: 'Introduction to TypeScript',
         line3: 'Introduction to Data Types',
-        line4: 'Let vs Const'
+        line4: 'Let vs Const',
+        line5: 'Introduction to Functions',
+        line6: 'Introduction to Parameters',
+        line7: 'Introduction to Classes'
       },
       {
         title: 'Lesson 02 - Features of Angular',
         line1: 'History of Angular',
         line2: 'Understanding Angular',
         line3: 'Set up Angular App',
-        line4: 'Angular Building Blocks'
-      }
-      , {
+        line4: 'Angular Building Blocks',
+        line5: 'Observables',
+        line6: 'Introduction to Reactive Forms',
+        line7: 'Introduction to Server Communication'
+      },
+      {
         title: 'Lesson 03 - Ngmodule',
-        line1: 'Angular Modulesr',
+        line1: 'Angular Modules',
         line2: 'Routing Module',
         line3: 'Feature Module',
-        line4: 'Sharing Module'
+        line4: 'Sharing Module',
+        line5: 'Systematically Arranging Components of App',
+        line6: 'Lesson 04 - Ng Unit Testing and Observables'
+      },
+      {
+        title: 'Introduction to Automated Testing',
+        line1: 'Types of Automated Testing',
+        line2: 'Introduction to Testing Tools',
+        line3: 'Introduction to Test Bed',
+        line4: 'Handling Component Dependencies',
+        line5: 'RxJS - Observable'
+      },
+      {
+        title: 'Lesson 05 - Bootstrap',
+        line1: 'Learning Objectives',
+        line2: 'Explain Responsive Web Design (RWD)',
+        line3: 'Understand the Bootstrap Grid System',
+        line4: 'Learn Bootstrap Components'
+      },
+      {
+        title: 'Lesson 06 - Binding and Events',
+        line1: 'Learning Objectives',
+        line2: 'What is Template Model?',
+        line3: 'How Angular Binding works and the Type of Bindings',
+        line4: 'Understand Angular Built-in Directives',
+        line5: 'Basics of Webpack and SystemJS'
+      },
+      {
+        title: 'Lesson 07 - Dependency Injection and Service',
+        line1: 'Learning Objectives',
+        line2: 'Understand Dependency Injection (DI)',
+        line3: 'Understand DI Application Programming Interface',
+        line4: 'Explain a Service',
+        line5: 'Describe How to Create a Service',
+        line6: 'Explain Injecting a Service'
+      },
+      {
+        title: 'Lesson 08 - Directives',
+        line1: 'Learning Objectives',
+        line2: 'Angular Directives',
+        line3: 'Types of Angular Directives',
+        line4: 'Built-in Angular Directives',
+        line5: 'Working with Custom Directives'
+      },
+      {
+        title: 'Lesson 09 - Pipes',
+        line1: 'Learning Objectives',
+        line2: 'What is Pipe in Angular',
+        line3: 'Understand How Built-in Pipes Work in Angular',
+        line4: 'Understand Angular Custom pipes',
+        line5: 'Quiz',
+        line6: 'Key Takeaways'
+      },
+      {
+        title: 'Lesson 10 - Forms',
+        line1: 'Learning Objectives',
+        line2: 'Angular Form Benefits',
+        line3: 'Template-Driven Approach',
+        line4: 'Model-Driven Approach',
+        line5: 'Angular Form Validation'
+      },
+      {
+        title: 'Lesson 11 - Routing',
+        line1: 'Learning Objectives',
+        line2: 'Understand How Angular Helps to Achieve SPA Using Routing',
+        line3: 'Define the Benefits of @NgModule',
+        line4: 'Identify Multiple Ways of Accessing Routes',
+        line5: 'Understand the Process of Routing Cycle'
+      },
+      {
+        title: 'Lesson 12 - HTTP, Promises, and Observables',
+        line1: 'Learning Objectives',
+        line2: 'Understand Working with RxJS',
+        line3: 'Understand Angular Interaction with HTTP GET',
+        line4: 'Describe the Process of Sending Data to the Server',
+        line5: 'Explain the Difference Between Promises and Observables'
+      },
+      {
+        title: 'Lesson 13 - Testing',
+        line1: 'Learning Objectives',
+        line2: 'Understand Tools and Setup',
+        line3: 'Understand Testing of Angular Class',
+        line4: 'Describe Testing Service',
+        line5: 'Understand Testing DOM',
+        line6: 'Explain End to End Testing'
       }
+
 
     ]
   },
@@ -48,29 +141,245 @@ export class SessionDetailPage {
     id: '8',
     syllabus: [
       {
+        title: 'Lesson 01 - ES6',
+        line1: 'Introduction',
+        line2: 'Array Helper Methods',
+        line3: 'Let and Const',
+        line4: 'Template Literals',
+        line5: 'Arrow Functions',
+        line6: 'Default Function Parameters',
+        line7: 'Rest and Spread',
+        line8: 'Destructuring',
+        line9: 'Classes',
+        line10: 'Promises',
+      },
+      {
+        title: 'Lesson 02 - Getting Started',
+        line1: 'What Is React',
+        line2: 'Environment Setup',
+        line3: 'ES6 Refresher - Part 1',
+        line4: 'ES6 Refresher - Part 2',
+        line5: 'ES6 Refresher - Part 3',
+        line6: 'Welcome To The Course',
+        line7: 'Create React App vs Manual Setup',
+        line8: 'Installing & Using The CLI',
+        line9: 'A Look At The Initial React Files & How ',
+        line10: 'They Work'
+      },
+      {
+        title: 'Lesson 03 - Introduction to Redux',
+        line1: 'Why Redux?',
+        line2: 'Core Concepts of Redux'
+      },
+      {
+        title: 'Lesson 04 - React & Redux',
+        line1: 'The React Redux Node Package',
+        line2: 'Provider Component',
+        line3: 'Connecting React Components with Redux Store',
+        line4: 'Reducer Composition',
+        line5: 'Normalization: Points to Keep in Mind When Designing a Redux Store',
+        line6: 'Redux Middleware'
+      },
+      {
+        title: 'Lesson 05 - Components, JSX & Props',
+        line1: 'Thinking In Components',
+        line2: 'What Is JSX',
+        line3: 'JSX Expressions',
+        line4: 'Creating Your First Component',
+        line5: 'Functional Components'
+      },
+      {
+        title: 'Lesson 06 - State & The Context API',
+        line1: 'State & The Context API',
+        },
+      {
+        title: 'Lesson 07 - Working With Forms',
+        line1: 'Changing State From Another Component',
+        line2: 'Using The Context API & Provider State',
+        line3: 'Adding A Context Reducer For Actions',
+        line4: 'Creating A Form With State',
+        line5: 'Controlled Components & onChange',
+        line6: 'TextInputGroup Component For DRY Code',
+        line7: 'Working With Props',
+        line8: 'Typechecking With PropTypes',
+        line9: 'CSS In React',
+        line10: 'Adding Bootstrap',
+        line11: 'Creating State',
+        line12: 'Events In React',
+        line13: 'Changing State',
+        line14: 'Uncontrolled Components & Refs',
+        line15: 'Form Submit Action To Context',
+        line16: 'Error Checking & Display',
+        line17: 'Course & Projects Outline',
+        line18: 'Self-Paced Curriculum'
+      },
+      {
+        title: 'Lesson 08 - Lifecycle, HTTP & Deployment',
+        line1: 'Lifecycle Methods',
+        line2: 'GET Requests',
+        line3: 'POST & DELETE Requests',
+        line4: 'Using AsyncAwait'
+      },
+      {
+        title: 'Lesson 09 - React Router v4',
+        line1: 'React Router Setup',
+        line2: 'Links, Params & Redirect',
+        line3: 'EditContact Component & Fetch Contact',
+        line4: 'PUT Request & Update Action',
+        line5: 'Deploy To Github Pages'
+      },
+      {
+        title: 'Lesson 10 - Learning Redux',
+        line1: 'A Note On Redux',
+        line2: 'App Starting Point For Redux',
+        line3: 'Redux Store Setup & Contact Reducer'
+      },
+      {
+        title: 'Lesson 11 - Redux Thunk & HTTP',
+        line1: 'GET Request Dispatch',
+        line2: 'POST & DELETE Request Dispatch',
+        line3: 'Get Single Contact For Edit',
+        line4: 'Connecting Components & Dispatching Actions',
+        line5: 'Creating A Separate Actions File',
+        line6: 'Delete & Add Contact Action',
+        line7: 'PUT Request & Dispatch',
+        line8: 'Deploy To Apache',
+        line9: 'App Intro & Demo',
+        line10: 'FirebaseFirestore Setup',
+      }
+
+    ]
+  },
+  {
+    courseName: 'JavaScript',
+    id: '17',
+    syllabus: [
+      {
         title: 'Lesson 01 - Features of TypeScript',
         line1: 'Introduction',
         line2: 'Introduction to TypeScript',
         line3: 'Introduction to Data Types',
         line4: 'Let vs Const'
-      },
-      {
-        title: 'Lesson 02 - Features of Angular',
-        line1: 'History of Angular',
-        line2: 'Understanding Angular',
-        line3: 'Set up Angular App',
-        line4: 'Angular Building Blocks'
       }
-      , {
-        title: 'Lesson 03 - Ngmodule',
-        line1: 'Angular Modulesr',
-        line2: 'Routing Module',
-        line3: 'Feature Module',
-        line4: 'Sharing Module'
-      }
-
     ]
-  }
+  },
+  {
+    courseName: 'MySQL',
+    id: '5',
+    syllabus: [
+      {
+        title: 'Lesson 01 - Features of TypeScript',
+        line1: 'Introduction',
+        line2: 'Introduction to TypeScript',
+        line3: 'Introduction to Data Types',
+        line4: 'Let vs Const'
+      }
+    ]
+  },
+  {
+    courseName: 'Java',
+    id: '18',
+    syllabus: [
+      {
+        title: 'Lesson 01 - Features of TypeScript',
+        line1: 'Introduction',
+        line2: 'Introduction to TypeScript',
+        line3: 'Introduction to Data Types',
+        line4: 'Let vs Const'
+      }
+    ]
+  },
+  {
+    courseName: 'SpringBoot',
+    id: '14',
+    syllabus: [
+      {
+        title: 'Lesson 01 - Features of TypeScript',
+        line1: 'Introduction',
+        line2: 'Introduction to TypeScript',
+        line3: 'Introduction to Data Types',
+        line4: 'Let vs Const'
+      }
+    ]
+  },
+  {
+    courseName: 'Python',
+    id: '',
+    syllabus: [
+      {
+        title: 'Lesson 01 - Features of TypeScript',
+        line1: 'Introduction',
+        line2: 'Introduction to TypeScript',
+        line3: 'Introduction to Data Types',
+        line4: 'Let vs Const'
+      }
+    ]
+  },
+  {
+    courseName: 'Html',
+    id: '9',
+    syllabus: [
+      {
+        title: 'Lesson 01 - Features of TypeScript',
+        line1: 'Introduction',
+        line2: 'Introduction to TypeScript',
+        line3: 'Introduction to Data Types',
+        line4: 'Let vs Const'
+      }
+    ]
+  },
+  {
+    courseName: 'CSS 3',
+    id: '9',
+    syllabus: [
+      {
+        title: 'Lesson 01 - Features of TypeScript',
+        line1: 'Introduction',
+        line2: 'Introduction to TypeScript',
+        line3: 'Introduction to Data Types',
+        line4: 'Let vs Const'
+      }
+    ]
+  },
+  {
+    courseName: 'AWS',
+    id: '15',
+    syllabus: [
+      {
+        title: 'Lesson 01 - Features of TypeScript',
+        line1: 'Introduction',
+        line2: 'Introduction to TypeScript',
+        line3: 'Introduction to Data Types',
+        line4: 'Let vs Const'
+      }
+    ]
+  },
+  {
+    courseName: 'MongoDB',
+    id: '13',
+    syllabus: [
+      {
+        title: 'Lesson 01 - Features of TypeScript',
+        line1: 'Introduction',
+        line2: 'Introduction to TypeScript',
+        line3: 'Introduction to Data Types',
+        line4: 'Let vs Const'
+      }
+    ]
+  },
+  {
+    courseName: 'Jquery',
+    id: '',
+    syllabus: [
+      {
+        title: 'Lesson 01 - Features of TypeScript',
+        line1: 'Introduction',
+        line2: 'Introduction to TypeScript',
+        line3: 'Introduction to Data Types',
+        line4: 'Let vs Const'
+      }
+    ]
+  },
   ];
 
   constructor(
@@ -103,6 +412,7 @@ export class SessionDetailPage {
     });
 
     this.selectedSyllabus = this.getSelectedSubjectSyllabus();
+
     console.log(this.selectedSyllabus);
   }
 
@@ -133,6 +443,8 @@ export class SessionDetailPage {
     const result = this.syllabusDetails.filter((syllabus) => {
       return syllabus.id === this.sessionId;
     });
+    const courseNameKey = 'courseName';
+    this.selectedCourseName = result[0][courseNameKey];
     const key = 'syllabus';
     return result[0][key];
   }
