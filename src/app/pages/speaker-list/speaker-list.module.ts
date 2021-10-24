950509d45036dfcbcbf789bb4b74fca7150ff8ef
RamakrishnanSelvaraj <2020mt93156@wilp.bits-pilani.ac.in>
2021-10-24 12:27:44 +0530

    Removed plugin - unused

--- src/app/pages/speaker-list/speaker-list.module.ts
+++ src/app/pages/speaker-list/speaker-list.module.ts
@@ -3,13 +3,12 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 // search module
-import { Ng2SearchPipeModule } from 'ng2-search-filter';
+
 import { SpeakerListPage } from './speaker-list';
 import { SpeakerListPageRoutingModule } from './speaker-list-routing.module';
 
 @NgModule({
   imports: [
-    Ng2SearchPipeModule,
     FormsModule,
     CommonModule,
     IonicModule,
